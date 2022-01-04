6f15a8661b1b324649de29bbe8afefee48693451
Alistair <alistairgarioch@gmail.com>
2022-01-04 13:27:54 +0000

    feat(constants): add breakpoint queries

--- src/constants.js
+++ src/constants.js
@@ -16,3 +16,9 @@ export const WEIGHTS = {
   medium: 600,
   bold: 800,
 };
+
+export const QUERIES = {
+  laptopMax: `(max-width: ${1300/16}rem)`,
+  tabletMax: `(max-width: ${950/16}rem)`,
+  phoneMax: `(max-width: ${600/16}rem)`
+}
